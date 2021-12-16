1d6387ccce916620f806eb59cf2f55a8ef0f037f
e-delsol <80672920+e-delsol@users.noreply.github.com>
2021-12-16 18:57:28 +0100

    Setting up the database

--- server.js
+++ server.js
@@ -1,38 +1,54 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import netflixData from "./data/netflix-titles.json";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+const Show = mongoose.model("Show", {
+	show_id: Number,
+	title: String,
+	director: String,
+	cast: String,
+	country: String,
+	date_added: String,
+	release_year: Number,
+	rating: String,
+	duration: String,
+	listed_in: String,
+	description: String,
+	type: String,
+});
+
+if (process.env.RESET_DB) {
+	const seedDatabase = () => {
+		// await Show.deleteMany({})
+
+		netflixData.forEach((item) => {
+			const newShow = new Show(item);
+			newShow.save();
+		});
+	};
+	seedDatabase();
+}
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+	res.send("Hello world");
+});
 
 // Start the server
 app.listen(port, () => {
-  // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+	// eslint-disable-next-line
+	console.log(`Server running on http://localhost:${port}`);
+});
